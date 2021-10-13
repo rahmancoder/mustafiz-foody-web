ebf948d1c4d3336759efc8cded4cf8ea5140ddb8
Mustafizur <sayhemrahman@gmail.com>
2021-10-14 05:33:40 +0600

    background added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
+import Background from './Components/Background/Background';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Background></Background>
 
     </div>
   );
--- src/Components/Background/Background.js
+++ src/Components/Background/Background.js
@@ -3,6 +3,10 @@ import React from 'react';
 const Background = () => {
     return (
         <div>
+            <img src="" alt="" />
+            <h1> Best food waiting for mustafiz belly</h1>
+            <input type="text" />
+            <button className="btn-danger">Search</button>
 
         </div>
     );
