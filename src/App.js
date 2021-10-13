6f3c3c436d98cc1b9c9a63fc4679c6ab79be1a9c
Mustafizur <sayhemrahman@gmail.com>
2021-10-14 05:30:36 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
 
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
+            <Navbar>
+                <Container>
+                    <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+                    <Navbar.Toggle />
+
+
+                    <Navbar.Collapse className="justify-content-end">
+
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+
+                        <Navbar.Text>
+                            Signed in as: <a href="#login">Mark Otto</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
 
         </div>
     );
